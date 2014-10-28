4884ab78f834120245f2249a41c7ea76eacaf186
Mixolidia Gautreaux <mixolidia@gmail.com>
2014-10-28 16:01:25 -0700

    Add Rails Bootstrap Forms gem, did you mean gem, changed header link to ruby link_to.

---
+++ app/controllers/albums_controller.rb
@@ -0,0 +1,7 @@
+class AlbumsController < ApplicationController
+
+    def new
+      @album = Album.new
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  get "/", to: 'home#index'
+  get "/", to: 'home#index', as: :home
   get "/albums/new", to: 'albums#new', as: :new
   post "/albums", to: "albums#create", as: :albums
 
